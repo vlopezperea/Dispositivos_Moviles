631c0c2a38107d73aa06f3e7704f0b8a7da29df7
vlopezperea <103610909+vlopezperea@users.noreply.github.com>
2025-08-07 20:23:45 -0300

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,61 @@
+
+const datos =[
+    {
+        id: 1,
+        nombre: "Picada Canchera",
+        imagen: require("@/assets/images/canchera2.jpg"),
+        precio: "$17.000 por 500g",
+        descripcion: "Quesos: Mar del Plata, azul, gruyerito  - Fiambres: salamin picado fino, longaniza calabresa",
+    },
+    {
+        id: 2,
+        nombre: "Picada Especial",
+        imagen: require("@/assets/images/especial.png"),
+        precio: "Chica: $17.100  Grande: $27.500",
+        descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito - Fiambres: jamon crudo, salame crespon, jamon cocido, salame milan, lomito de cerdo ahumado, bondiola - Aceitunas: verdes rellenas",
+
+    },
+    {
+        id: 3,
+        imagen: require("@/assets/images/picadaPremiun1.jpg"),
+        descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito, sardo, gouda, camembert - Frutos secos: almendras, castañas de caju, nueces",
+
+    },
+    {
+        id: 4,
+        imagen: require("@/assets/images/picadaPremium2.jpg"),
+        descripcion: "Quesos: Mar del Plata, azul, gruyerito, sardo, reggianito, camembert - Fiambres: jamon crudo, jamon cocido natural, lomo de cerdo horneado, bondiola - Frutos secos: almendras, castañas de caju, pistachos",
+    },
+    {
+        id: 5,
+        imagen: require("@/assets/images/tablaQuesos.jpg"),
+        descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito",
+    },
+    {
+        id: 6,
+        imagen: require("@/assets/images/quesoCremoso.jpg"),
+        descripcion: " ",
+    },
+    {
+        id: 7,
+        imagen: require("@/assets/images/lunes.png"),
+        descripcion: " ",
+    },
+    {
+        id: 8,
+        imagen: require("@/assets/images/martes.png"),
+        descripcion: " ",
+    },
+    {
+        id: 9,
+        imagen: require("@/assets/images/miercoles.png"),
+        descripcion: " ",
+    },
+    {
+        id: 10,
+        imagen: require("@/assets/images/jueves.png"),
+        descripcion: " ",
+    },
+      
+]
+export default datos
