a92eada3dff56f1f0cecda1a0157c98adcd6c03b
vlopezperea <103610909+vlopezperea@users.noreply.github.com>
2025-08-07 20:26:16 -0300

    Add files via upload

--- components/datos.js
+++ components/datos.js
@@ -2,54 +2,58 @@
 const datos =[
     {
         id: 1,
-        imagen: "@/assets/images/canchera.jpg",
+        nombre: "Picada Canchera",
+        imagen: require("@/assets/images/canchera2.jpg"),
+        precio: "$17.000 por 500g",
         descripcion: "Quesos: Mar del Plata, azul, gruyerito  - Fiambres: salamin picado fino, longaniza calabresa",
     },
     {
         id: 2,
-        imagen: "@/assets/images/picadaEspecial.jpg",
+        nombre: "Picada Especial",
+        imagen: require("@/assets/images/especial.png"),
+        precio: "Chica: $17.100  Grande: $27.500",
         descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito - Fiambres: jamon crudo, salame crespon, jamon cocido, salame milan, lomito de cerdo ahumado, bondiola - Aceitunas: verdes rellenas",
 
     },
     {
         id: 3,
-        imagen: "@/assets/images/picadaPremium1.jpg",
+        imagen: require("@/assets/images/picadaPremiun1.jpg"),
         descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito, sardo, gouda, camembert - Frutos secos: almendras, castañas de caju, nueces",
 
     },
     {
         id: 4,
-        imagen: "@/assets/images/picadaPremium2.jpg",
+        imagen: require("@/assets/images/picadaPremium2.jpg"),
         descripcion: "Quesos: Mar del Plata, azul, gruyerito, sardo, reggianito, camembert - Fiambres: jamon crudo, jamon cocido natural, lomo de cerdo horneado, bondiola - Frutos secos: almendras, castañas de caju, pistachos",
     },
     {
         id: 5,
-        imagen: "@/assets/images/tablaQuesos.jpg",
+        imagen: require("@/assets/images/tablaQuesos.jpg"),
         descripcion: "Quesos: Mar del Plata, azul, gruyerito, reggianito",
     },
     {
         id: 6,
-        imagen: "@/assets/images/quesoCremoso.jpg",
+        imagen: require("@/assets/images/quesoCremoso.jpg"),
         descripcion: " ",
     },
     {
         id: 7,
-        imagen: "@/assets/images/lunes.png",
+        imagen: require("@/assets/images/lunes.png"),
         descripcion: " ",
     },
     {
         id: 8,
-        imagen: "@/assets/images/martes.png",
+        imagen: require("@/assets/images/martes.png"),
         descripcion: " ",
     },
     {
         id: 9,
-        imagen: "@/assets/images/miercoles.png",
+        imagen: require("@/assets/images/miercoles.png"),
         descripcion: " ",
     },
     {
         id: 10,
-        imagen: "@/assets/images/jueves.png",
+        imagen: require("@/assets/images/jueves.png"),
         descripcion: " ",
     },
       
